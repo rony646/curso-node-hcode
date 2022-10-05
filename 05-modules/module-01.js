4806a305df9f8389243db11715499992ffd3abea
Rony Silva <rony.silva@grupoboticario.com.br>
2022-10-05 17:45:07 -0300

    custom modules

---
+++ 05-modules/module-01.js
@@ -0,0 +1,14 @@
+const ocult = () => {
+  console.log('Executando funcao oculta');
+};
+
+executa = () => {
+  console.log('Executando funcao executa')
+};
+
+const welcome = `Bem vindo ao module-02.js!`;
+
+module.exports = {
+  executa,
+  welcome,
+};
---
+++ 05-modules/module-02.js
@@ -0,0 +1,5 @@
+const { executa, welcome } = require('./module-01');
+
+//executando um modulo importado
+executa()
+console.log(welcome)
